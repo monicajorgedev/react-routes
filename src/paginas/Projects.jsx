a1356f7d84e02467dd1f718af1fa5508902e43b3
monicajorgedev <monicajorge.dev@gmail.com>
2024-10-23 09:27:58 +0200

    ejercicio terminado

---
+++ src/paginas/Projects.jsx
@@ -0,0 +1,30 @@
+import './Projects.css'
+import React, {useState, useEffect} from "react";
+import { Link } from 'react-router-dom';
+import projects from "../data/projects.js";
+
+function Projects() {
+    return (
+        <>
+            <Link to={'/'}>Home</Link>
+            <h1>Projects</h1>
+            <div className="container">
+             {projects.map((project)=> {
+                return (
+                <ul key={project.id}>
+                    <li>ID: {project.id}</li>
+                    <li>Name: {project.name}</li>
+                    <li><img src={project.image} alt={project.name}></img></li>
+                    <li>Description: {project.description}</li>
+                    <li><a href={project.url}>{project.url}</a></li>
+                </ul>
+                )
+             })}
+             </div>
+        </>
+    )
+}
+
+
+
+export default Projects
---
+++ src/paginas/Resume.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from "react";
+import { Link } from 'react-router-dom';
+import {studies, experiences} from '../data/resume.js'
+
+console.log(studies)
+console.log(experiences)
+
+function Resume() {
+    return (
+        <>
+            <Link to={'/'}>Home</Link>
+            <h1>Resume</h1>
+            <div className="container">
+                <div>
+                    <h2>Studies</h2>
+                    {studies.map((study)=> {
+                        return (
+                        <ul key={study.id}>
+                            <li>ID: {study.id}</li>
+                            <li>title: {study.title}</li>
+                            <li>institución: {study.institucion}</li>
+                            <li>date: {study.date}</li>
+                        </ul>
+                        )
+                    })}
+                </div>
+                <div>
+                    <h2>Experiences</h2>
+                    {experiences.map((experience)=> {
+                        return (
+                        <ul key={experience.id}>
+                            <li>ID: {experience.id}</li>
+                            <li>title: {experience.title}</li>
+                            <li>company: {experience.institucion}</li>
+                            <li>date: {experience.date}</li>
+                        </ul>
+                        )
+                    })}
+                </div>
+             </div>
+        </>
+    )
+}
+
+export default Resume
